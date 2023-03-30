b4bec57a8757dbf4c5ccfde25022709264f33013
Ivan Lisitskii <ivan.janson@gmail.com>
2023-03-31 01:11:50 +0300

    Update string helper

--- app/Service/Util/StringHelper.php
+++ app/Service/Util/StringHelper.php
@@ -6,7 +6,7 @@ class StringHelper
 {
 
     /**
-     * сколько времени назад
+     * How much time ago formatter
      *
      * @param $time
      *
@@ -14,7 +14,7 @@ class StringHelper
      */
     public static function lvdateBack($time)
     {
-        if(!is_numeric($time)) {
+        if (!is_numeric($time)) {
             $time = strtotime($time);
         }
 
@@ -23,29 +23,27 @@ class StringHelper
         $days = floor($sec / 86400);
         $hours = floor($sec / 3600);
         $minutes = floor(($sec - ($hours * 3600)) / 60);
-        $hoursRus = ''; // 1,21 час, 2,22,23,24,25 часа, часов
-        $minsRus = '';
 
         if ($days) {
-            return self::plural( $days, ' день назад',' дня назад',' дней назад');
+            return self::plural( $days, ' day ago',' days ago');
         }
         if ($hours) {
-            return self::plural($hours, ' час назад',' часа назад',' часов назад', $hours);
+            return self::plural($hours, ' hour ago',' hours ago', null, $hours);
         }
         if ($minutes) {
-            return self::plural($minutes, ' минуту назад',' минуты назад',' минут назад', $minutes);
+            return self::plural($minutes, ' minute ago',' minutes ago', null, $minutes);
         }
         if ($sec > 0 && !$hours && !$minutes) {
-            return $sec . ' сек. назад';
+            return $sec . ' sec ago';
         }
         if ($sec == 0) {
-            return 'только что';
+            return 'right now';
         }
     }
 
     public static function plural($count, $form1, $form2 = null, $form3 = null, $nullForm = null, $printNumber = true)
     {
-        if(!$count && !is_null($nullForm)){
+        if (!$count && !is_null($nullForm)){
             return ($printNumber ? (int) $count.' ' : '') . $nullForm;
         }
         $form2 = is_null($form2) ? $form1 : $form2;
@@ -57,11 +55,10 @@ class StringHelper
     private static function getNumberDependedString($number, $titles, $printNumber = true)
     {
         $absNumber = abs($number);
-            $cases = array (2, 0, 1, 1, 1, 2);
-            $form = $titles[ ($absNumber % 100 > 4 && $absNumber % 100 < 20) ?  2 : $cases[min($absNumber % 10, 5)] ];
+        $cases = array (2, 0, 1, 1, 1, 2);
+        $form = $titles[ ($absNumber % 100 > 4 && $absNumber % 100 < 20) ?  2 : $cases[min($absNumber % 10, 5)] ];
 
         return ($printNumber ? $number . ' ' : '') . $form;
     }
-
 }
  
